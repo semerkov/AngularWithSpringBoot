029d0fec094ad82b593664d359cc809c557dfa54
Ricardo Faria <ricardo.faria@outlook.com.br>
2015-06-04 20:56:54 -0300

    Create startup listener to fill db user if db is empty

--- server/src/main/java/com/rf/ApplicationStartupListener.java
+++ server/src/main/java/com/rf/ApplicationStartupListener.java
@@ -1,30 +1,23 @@
 package com.rf;
 
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
-import java.io.LineNumberReader;
-import java.net.URL;
-import java.sql.Connection;
-import java.util.List;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import javax.imageio.ImageIO;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rf.user.persistence.UserRepository;
 
 @Component
-public class ApplicationStartupListener implements
-		ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationStartupListener implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired(required = true)
 	private UserRepository repository;
@@ -37,20 +30,19 @@ public class ApplicationStartupListener implements
 
 		if (repository.count() == 0) {
 			try {
-				/*System.out.println("Inserting all users in import.sql");
-				
-				final ResourceDatabasePopulator rdp = new ResourceDatabasePopulator();
-				rdp.addScript(new ClassPathResource("/import.sql"));
-				
-				manager.getTransaction().begin();
-				Connection connection = manager.unwrap(java.sql.Connection.class);
-				rdp.populate(connection);
-				manager.getTransaction().commit();
-				System.out.println("DONE");*/
+				System.out.println("Inserting all users in import.sql");
+
+				URI uri = ApplicationStartupListener.class.getResource("/import.sql").toURI();
+				for (String line : Files.readAllLines(Paths.get(uri), Charset.defaultCharset())) {
+					Query query = manager.createNativeQuery(line);
+					query.executeUpdate();
+				}
+
+				System.out.println("DONE");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }
