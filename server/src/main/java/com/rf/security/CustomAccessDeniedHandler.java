83717f1506425866b46964f23e264920bd9b4753
Ricardo Faria <ricardo.faria@outlook.com.br>
2015-06-18 14:38:11 -0300

    Changed blocked response to 403

--- server/src/main/java/com/rf/security/CustomAccessDeniedHandler.java
+++ server/src/main/java/com/rf/security/CustomAccessDeniedHandler.java
@@ -15,9 +15,9 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
 			throws IOException, ServletException {
-		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         PrintWriter writer = response.getWriter();
-        writer.println("HTTP Status 401 - " + accessDeniedException.getMessage());
+        writer.println("HTTP Status 403 - " + accessDeniedException.getMessage());
 	}
 
 }
